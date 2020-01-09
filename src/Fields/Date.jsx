81dfda8e5a1f4fe03b70a8c3f9dc71fdbe4d4186
yaroslaw_gutnyk <gytjarek@gmail.com>
2020-01-09 11:20:17 +0200

    make possible to change date format in dropdown type

--- src/Fields/Date.jsx
+++ src/Fields/Date.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import DatePicker from 'react-datepicker';
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
 
 export default ({
   field,
@@ -32,23 +32,46 @@ export default ({
     customName,
     dateType,
     dateFormat,
-    defaultValue,
+    defaultValue
   } = field;
 
   const {
- Input = 'input', Label = 'label', Box = 'div', DatePicker: SdatePicker = 'div' 
-} =    styledComponents || false;
+    Input = "input",
+    Label = "label",
+    Box = "div",
+    DatePicker: SdatePicker = "div"
+  } = styledComponents || false;
 
   // conver date format
-  const format = dateFormat && dateFormat === 'dmy' ? 'dd/MM/yyyy' : false;
+  const format = dateFormat && dateFormat === "dmy" ? "dd/MM/yyyy" : false;
   let selectedValue = defaultValue ? new Date(defaultValue) : false;
   if (format && defaultValue) {
-    const dateParts = defaultValue.split('/');
+    const dateParts = defaultValue.split("/");
     const dateObject = new Date(+dateParts[2], dateParts[1] - 1, +dateParts[0]);
     selectedValue = dateObject;
   }
 
   const [startDate, setDate] = useState(selectedValue);
+
+  const getFormattedInputs = items => {
+    // 0 - MM
+    // 1 - DD
+    // 2 - YYYY
+    switch (dateFormat) {
+      case "dmy":
+      case "dmy_dash":
+      case "dmy_dot":
+        return [items[1], items[0], items[2]];
+      case "ymd_slash":
+      case "ymd_dash":
+      case "ymd_dot":
+        return [items[2], items[0], items[1]];
+      default:
+        return items;
+    }
+  };
+
+  const formatedInputs = getFormattedInputs(inputs);
 
   return (
     <Box
@@ -58,18 +81,21 @@ export default ({
           ? `form-field error ${cssClass}`
           : `form-field ${cssClass}`
       }
-      style={{ display: hideField ? 'none' : undefined }}
+      style={{ display: hideField ? "none" : undefined }}
     >
-      <Label htmlFor={`input_${formId}_${id}`} className={`gf-label ${labelPlacement}`}>
+      <Label
+        htmlFor={`input_${formId}_${id}`}
+        className={`gf-label ${labelPlacement}`}
+      >
         {label}
         {isRequired ? <abbr>*</abbr> : null}
       </Label>
       <div className={type}>
-        {descriptionPlacement === 'above' && description ? (
+        {descriptionPlacement === "above" && description ? (
           description && <div className="description">{description}</div>
         ) : (
           <React.Fragment>
-            {dateType && dateType === 'datepicker' ? (
+            {dateType && dateType === "datepicker" ? (
               <React.Fragment>
                 <SdatePicker className="ginput_container ginput_container_date">
                   <DatePicker
@@ -78,28 +104,28 @@ export default ({
                     type="text"
                     className="datepicker hasDatepicker"
                     selected={startDate}
-                    onChange={(date) => {
+                    onChange={date => {
                       setDate(date);
                       updateForm(
                         {
                           target: {
-                            value: date,
-                          },
+                            value: date
+                          }
                         },
-                        field,
+                        field
                       );
                       setTouched(id);
                       unsetError(id);
                       setFocusClass(date);
                     }}
-                    onBlur={(e) => {
+                    onBlur={e => {
                       updateForm(
                         {
                           target: {
-                            value: startDate,
-                          },
+                            value: startDate
+                          }
                         },
-                        field,
+                        field
                       );
                       setTouched(id);
                       unsetError(id);
@@ -119,8 +145,8 @@ export default ({
               </React.Fragment>
             ) : (
               <React.Fragment>
-                {inputs
-                  && inputs.map((item, index) => (
+                {formatedInputs &&
+                  formatedInputs.map((item, index) => (
                     <div className={type} key={item.id}>
                       <Input
                         id={`input_${formId}_${id}_${index}`}
@@ -130,41 +156,49 @@ export default ({
                         step="1"
                         min="1"
                         max={
-                          item.label === 'MM'
+                          item.label === "MM"
                             ? 12
-                            : item.label === 'DD'
-                              ? 31
-                              : new Date().getFullYear() + 1
+                            : item.label === "DD"
+                            ? 31
+                            : new Date().getFullYear() + 1
                         }
-                        maxLength={item.label === 'YYYY' ? 4 : 2}
+                        maxLength={item.label === "YYYY" ? 4 : 2}
                         value={item.value}
-                        onBlur={(event) => {
+                        onBlur={event => {
                           field.subId = index;
                           field.dateLabel = item.label;
                           updateForm(event, field);
                           setTouched(id);
                           unsetError(id);
-                          setFocusClass(item.value !== '');
+                          setFocusClass(item.value !== "");
                         }}
                         onFocus={() => setFocusClass(true)}
                       />
-                      <label htmlFor={`input_${formId}_${id}_${index}`} className="hide-label">
+                      <label
+                        htmlFor={`input_${formId}_${id}_${index}`}
+                        className="hide-label"
+                      >
                         {item.label}
                       </label>
-                      {validationMessage
-                        && touched
-                        && validationMessage[index]
-                        && index === validationMessage[index].index
-                        && validationMessage[index].message && (
-                        <span className="error-message" id={`error_${formId}_${item.id}`}>
-                          {validationMessage[index].message}
-                        </span>
-                      )}
+                      {validationMessage &&
+                        touched &&
+                        validationMessage[index] &&
+                        index === validationMessage[index].index &&
+                        validationMessage[index].message && (
+                          <span
+                            className="error-message"
+                            id={`error_${formId}_${item.id}`}
+                          >
+                            {validationMessage[index].message}
+                          </span>
+                        )}
                       {error && <span className="error-message">{error}</span>}
                     </div>
                   ))}
 
-                {description && <div className="description">{description}</div>}
+                {description && (
+                  <div className="description">{description}</div>
+                )}
               </React.Fragment>
             )}
           </React.Fragment>
