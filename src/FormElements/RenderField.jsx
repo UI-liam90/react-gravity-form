f43ea22e317ece8691bc17755c6a2875ed90a9a6
yaroslaw_gutnyk <gytjarek@gmail.com>
2020-04-16 16:15:47 +0300

    fix css class

--- src/FormElements/RenderField.jsx
+++ src/FormElements/RenderField.jsx
@@ -1,35 +1,36 @@
-import React, { useState } from 'react';
-import * as FormFields from '../Fields';
+import React, { useState } from "react";
+import * as FormFields from "../Fields";
 
 const frac2dec = (fraction) => {
   /* assumes fraction is in the form  1-1/2 or 1 1/2 */
   /* doesn't work on negative numbers */
-  let fractionParts = fraction.split('-');
+  let fractionParts = fraction.split("-");
   if (fractionParts.length === 1) {
     /* try space as divider */
-    fractionParts = fraction.split(' ');
+    fractionParts = fraction.split(" ");
   }
 
-  if (fractionParts.length > 1 && fraction.indexOf('/') !== -1) {
+  if (fractionParts.length > 1 && fraction.indexOf("/") !== -1) {
     const integer = parseInt(fractionParts[0]);
-    const decimalParts = fractionParts[1].split('/');
+    const decimalParts = fractionParts[1].split("/");
     const decimal = parseInt(decimalParts[0]) / parseInt(decimalParts[1]);
 
     return integer + decimal;
   }
-  if (fraction.indexOf('/') !== -1) {
-    const decimalParts = fraction.split('/');
+  if (fraction.indexOf("/") !== -1) {
+    const decimalParts = fraction.split("/");
     const decimal = parseInt(decimalParts[0]) / parseInt(decimalParts[1]);
     return decimal;
   }
   return parseInt(fraction);
 };
 
-const formatComponentName = string => string.charAt(0).toUpperCase() + string.slice(1);
+const formatComponentName = (string) =>
+  string.charAt(0).toUpperCase() + string.slice(1);
 const formatWidthFromCss = (cssClass) => {
   if (!cssClass) return {};
-  const widthStarts = cssClass.indexOf('[');
-  const widthEnds = cssClass.indexOf(']');
+  const widthStarts = cssClass.indexOf("[");
+  const widthEnds = cssClass.indexOf("]");
 
   if (widthStarts === -1 || widthEnds === -1) {
     return {};
@@ -37,12 +38,12 @@ const formatWidthFromCss = (cssClass) => {
 
   const width = cssClass
     .substring(widthStarts + 1, widthEnds)
-    .split(',')
-    .map(item => frac2dec(item.replace(/\s/g, '')));
+    .split(",")
+    .map((item) => frac2dec(item.replace(/\s/g, "")));
 
   const cleanedCssClass = `${cssClass.replace(
     cssClass.substring(widthStarts, widthEnds + 1),
-    '',
+    ""
   )} custom-width`;
 
   return {
@@ -76,18 +77,28 @@ const RenderField = ({
     field.cssClass = cleanedCssClass;
     field.width = width;
   }
-  const value = formValues[field.id] ? formValues[field.id].value : field.defaultValue;
+  const value = formValues[field.id]
+    ? formValues[field.id].value
+    : field.defaultValue;
   const [fieldClassName, setFieldClassName] = useState(
-    `${field.cssClass}${field.type === 'select' ? value.value !== '' ? 'filled' : '' : value && value !== '' ? 'filled' : ''}`,
+    `${field.cssClass}${
+      field.type === "select"
+        ? value.value !== ""
+          ? " filled"
+          : ""
+        : value && value !== ""
+        ? " filled"
+        : ""
+    }`
   );
 
   const setFocusClass = (action) => {
     if (action) {
-      if (fieldClassName.indexOf(' filled') === -1) {
+      if (fieldClassName.indexOf(" filled") === -1) {
         setFieldClassName(`${fieldClassName} filled`);
       }
     } else {
-      setFieldClassName(fieldClassName.replace(' filled', ''));
+      setFieldClassName(fieldClassName.replace(" filled", ""));
     }
   };
 
@@ -97,7 +108,9 @@ const RenderField = ({
       field={field}
       value={value}
       updateForm={(event, field) => updateForm(event, field)}
-      validationMessage={formValues[field.id] ? formValues[field.id].valid : false}
+      validationMessage={
+        formValues[field.id] ? formValues[field.id].valid : false
+      }
       submitFailed={submitFailed}
       submitSuccess={submitSuccess}
       touched={touched[field.id]}
@@ -110,7 +123,7 @@ const RenderField = ({
       isNextDisabled={isNextDisabled}
       hideField={formValues[field.id] ? formValues[field.id].hideField : false}
       saveStateToHtmlField={
-        field.type === 'html' && field.cssClass.indexOf('set-state') !== -1
+        field.type === "html" && field.cssClass.indexOf("set-state") !== -1
           ? { formValues, saveStateToHtmlField }
           : false
       }
