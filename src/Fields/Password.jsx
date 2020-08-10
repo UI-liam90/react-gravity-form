37da4bcfbdbf3b8a6007e42677060da2f16d1227
yaroslaw_gutnyk <gytjarek@gmail.com>
2020-08-10 14:35:29 +0300

    render password field placeholder as label text

--- src/Fields/Password.jsx
+++ src/Fields/Password.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export default ({
   field,
@@ -32,17 +32,18 @@ export default ({
     minPasswordStrength,
   } = field;
 
-  const { Input = 'input', Label = 'label', Box = 'div' } = styledComponents || false;
+  const { Input = "input", Label = "label", Box = "div" } =
+    styledComponents || false;
 
   const [passwords, setPasswords] = useState(inputs);
-  const [passwordStrength, setPasswordStrength] = useState('blank');
+  const [passwordStrength, setPasswordStrength] = useState("blank");
 
   const setFocusClass = (action, i) => {
     const pass = { ...passwords };
     if (action) {
-      pass[i].cssClass = 'filled';
+      pass[i].cssClass = "filled";
     } else {
-      pass[i].cssClass = '';
+      pass[i].cssClass = "";
     }
     setPasswords(pass);
   };
@@ -57,10 +58,10 @@ export default ({
     let natLog;
     let score;
 
-    if (password1.length <= 0) return 'blank';
+    if (password1.length <= 0) return "blank";
 
     // password < 4
-    if (password1.length < 4) return 'short';
+    if (password1.length < 4) return "short";
 
     if (password1.match(/[0-9]/)) symbolSize += 10;
     if (password1.match(/[a-z]/)) symbolSize += 26;
@@ -70,11 +71,11 @@ export default ({
     natLog = Math.log(Math.pow(symbolSize, password1.length));
     score = natLog / Math.LN2;
 
-    if (score < 40) return 'bad';
+    if (score < 40) return "bad";
 
-    if (score < 56) return 'good';
+    if (score < 56) return "good";
 
-    return 'strong';
+    return "strong";
   };
 
   const setGFPwdStrength = (password) => {
@@ -90,17 +91,19 @@ export default ({
           ? `form-field error ${cssClass}`
           : `form-field ${cssClass}`
       }
-      style={{ display: hideField ? 'none' : undefined }}
+      style={{ display: hideField ? "none" : undefined }}
     >
       <div className={type}>
         <Label className={`gf-label ${labelPlacement}`}>
           {label}
           {isRequired ? <abbr>*</abbr> : null}
         </Label>
-        {descriptionPlacement === 'above' && description && (
+        {descriptionPlacement === "above" && description && (
           <div
             className="description"
-            dangerouslySetInnerHTML={{ __html: description.replace(/\n/g, '<br />') }}
+            dangerouslySetInnerHTML={{
+              __html: description.replace(/\n/g, "<br />"),
+            }}
           />
         )}
         <div className="ginput_container ginput_container_password">
@@ -112,15 +115,23 @@ export default ({
                   <span
                     key={`input_${formId}_${input.id}`}
                     className={`${
-                      inputs.length > 1 ? `ginput_${i === 0 ? 'left' : 'right'}` : 'medim'
-                    } ${passwords[i].cssClass ? passwords[i].cssClass : ''}`}
+                      inputs.length > 1
+                        ? `ginput_${i === 0 ? "left" : "right"}`
+                        : "medim"
+                    } ${passwords[i].cssClass ? passwords[i].cssClass : ""}`}
                   >
                     <Input
                       id={`input_${formId}_${input.id}_${i}`}
-                      name={customName || `input_${id}${i === 1 ? `_${i + 1}` : ''}`}
+                      name={
+                        customName || `input_${id}${i === 1 ? `_${i + 1}` : ""}`
+                      }
                       type={type}
-                      value={value && value[i] && value[i].val ? value[i].val : ''}
-                      placeholder={input.placeholder ? input.placeholder : placeholder}
+                      value={
+                        value && value[i] && value[i].val ? value[i].val : ""
+                      }
+                      placeholder={
+                        input.placeholder ? input.placeholder : placeholder
+                      }
                       required={isRequired}
                       autoComplete="off"
                       onChange={(event) => {
@@ -135,14 +146,22 @@ export default ({
                         field.subId = i;
                         updateForm(event, field);
                         setTouched(id);
-                        setFocusClass(value && value[i] && value[i].val && value[i].val !== '', i);
+                        setFocusClass(
+                          value &&
+                            value[i] &&
+                            value[i].val &&
+                            value[i].val !== "",
+                          i
+                        );
                       }}
                       onFocus={() => setFocusClass(true, i)}
                       aria-label={label}
                       aria-describedby={`error_${formId}_${input.id}_${i}`}
                       aria-invalid={!!validationMessage && touched}
                     />
-                    <label htmlFor={`input_${formId}_${input.id}_${i}`}>{input.label}</label>
+                    <label htmlFor={`input_${formId}_${input.id}_${i}`}>
+                      {input.placeholder || input.label}
+                    </label>
                   </span>
                 )
             )}
@@ -154,10 +173,12 @@ export default ({
             dangerouslySetInnerHTML={{ __html: validationMessage || error }}
           />
         )}
-        {descriptionPlacement !== 'above' && description && (
+        {descriptionPlacement !== "above" && description && (
           <div
             className="description"
-            dangerouslySetInnerHTML={{ __html: description.replace(/\n/g, '<br />') }}
+            dangerouslySetInnerHTML={{
+              __html: description.replace(/\n/g, "<br />"),
+            }}
           />
         )}
         {!((validationMessage && touched) || error) && passwordStrengthEnabled && (
@@ -166,9 +187,9 @@ export default ({
               id={`input_${formId}_${id}_strength_indicator`}
               className={`gfield_password_strength ${passwordStrength}`}
             >
-              {passwordStrength && passwordStrength !== 'blank'
+              {passwordStrength && passwordStrength !== "blank"
                 ? passwordStrength
-                : 'Strength indicator'}
+                : "Strength indicator"}
             </div>
             <input
               type="hidden"
@@ -183,4 +204,3 @@ export default ({
     </Box>
   );
 };
-
