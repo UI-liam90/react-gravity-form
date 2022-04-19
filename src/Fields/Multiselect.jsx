69094e8b14cfacfa13ef660fc2e8a76944f17608
yaroslaw_gutnyk <gytjarek@gmail.com>
2022-04-19 14:13:04 +0300

    fix multiselect issue

--- src/Fields/Multiselect.jsx
+++ src/Fields/Multiselect.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import Select from 'react-select';
-import InputLabel from '../FormElements/InputLabel'
+import React, { useState } from "react";
+import Select from "react-select";
+import InputLabel from "../FormElements/InputLabel";
 
 export default ({
   field,
@@ -8,6 +8,7 @@ export default ({
   validationMessage,
   touched,
   setTouched,
+  setFocusClass,
   updateForm,
   hideField,
   styledComponents,
@@ -44,18 +45,20 @@ export default ({
       },
     ];
   } else {
-    preselected = '';
+    preselected = "";
   }
   // Handle State
   const [selectedOption, selectOption] = useState(value || preselected);
   // Handle change
-  const handleChange = (option) => {
+  const handleChange = option => {
     selectOption(option);
     const event = {
       target: {
-        value: option,
+        value:
+          option && option.length > 0 ? option.map(item => item.value) : "",
       },
     };
+
     updateForm(event, field);
     unsetError(id);
   };
@@ -63,13 +66,27 @@ export default ({
   const handleBlur = () => {
     const event = {
       target: {
-        value: selectedOption,
+        value:
+          selectedOption && selectedOption.length > 0
+            ? selectedOption.map(item => item.value)
+            : "",
       },
     };
+    console.log("event", event);
     updateForm(event, field);
     setTouched(id);
   };
-  const { SelectStyles, Label = 'label', Box = 'div' } = styledComponents || false;
+  const {
+    ReactSelect,
+    Label = "label",
+    Box = "div",
+  } = styledComponents || false;
+
+  const RSelect = ReactSelect || Select;
+  const selectValues = [...selectedOption];
+  if (selectedOption && selectOption.length > 0) {
+    selectValues.shift();
+  }
   return (
     <Box
       width={width}
@@ -78,7 +95,7 @@ export default ({
           ? `form-field error ${cssClass}`
           : `form-field ${cssClass}`
       }
-      style={{ display: hideField ? 'none' : undefined }}
+      style={{ display: hideField ? "none" : undefined }}
     >
       <div className={type}>
         <InputLabel
@@ -92,21 +109,41 @@ export default ({
         {descriptionPlacement === "above" && description && (
           <div className="description">{description}</div>
         )}
-        <Select
-          name={customName || `input_${id}[]`}
+        <RSelect
+          name={customName || `input_${id}`}
           required={isRequired}
           value={selectedOption}
-          onChange={(option) => {
+          onChange={option => {
             handleChange(option, field);
+            unsetError(id);
           }}
           onBlur={() => handleBlur()}
+          onFocus={() => setFocusClass(true)}
           placeholder={placeholder}
           options={options}
+          autoFocus={false}
           isMulti
           inputId={`input_${formId}_${id}`}
-          styles={SelectStyles}
         />
-        {descriptionPlacement !== "above" && description && <div className="description">{description}</div>}
+        <input
+          type="hidden"
+          name={`input_${id}[]`}
+          required={isRequired}
+          value={
+            selectedOption && selectedOption[0]?.value
+              ? selectedOption[0].value
+              : ""
+          }
+        />
+        {selectValues &&
+          selectValues.length > 0 &&
+          selectValues.map(item => (
+            <input type="hidden" name={`input_${id}[]`} value={item.value} />
+          ))}
+
+        {descriptionPlacement !== "above" && description && (
+          <div className="description">{description}</div>
+        )}
         {((validationMessage && touched) || error) && (
           <span className="error-message" id={`error_${formId}_${id}`}>
             {validationMessage || error}
