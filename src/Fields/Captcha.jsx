c8a8b10417e9437fcc42da48fbd8b4b646a92288
yaroslaw_gutnyk <gytjarek@gmail.com>
2022-09-16 10:32:58 +0300

    tweak captcha field; show error;

--- src/Fields/Captcha.jsx
+++ src/Fields/Captcha.jsx
@@ -1,19 +1,69 @@
 import React from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 
-const Captcha = ({ field, validationMessage, cssClass, sitekey, ...props }) => {
-  const { id, captchaTheme, formId, captchaLanguage } = field;
+const Captcha = ({
+  field,
+  validationMessage,
+  error,
+  sitekey,
+  styledComponents,
+  setTouched,
+  unsetError,
+  touched,
+  ...props
+}) => {
+  const {
+    id,
+    captchaTheme,
+    formId,
+    captchaLanguage,
+    width,
+    cssClass,
+    description,
+    descriptionPlacement,
+  } = field;
+
+  const { Box = "div" } = styledComponents || false;
+
   if (!sitekey) return null;
 
   return (
-    <div className={`captcha ${cssClass}`}>
-      <ReCAPTCHA sitekey={sitekey} theme={captchaTheme} hl={captchaLanguage} />
-      {validationMessage && (
+    <Box
+      width={width}
+      className={
+        (validationMessage && touched) || error
+          ? `form-field captcha error ${cssClass}`
+          : `form-field captcha ${cssClass}`
+      }
+      style={{ display: hideField ? "none" : undefined }}
+    >
+      {descriptionPlacement === "above" && description && (
+        <div
+          className="description"
+          dangerouslySetInnerHTML={{ __html: description }}
+        />
+      )}
+      <ReCAPTCHA
+        sitekey={sitekey}
+        theme={captchaTheme}
+        hl={captchaLanguage}
+        onChange={() => {
+          setTouched(id);
+          unsetError(id);
+        }}
+      />
+      {descriptionPlacement !== "above" && description && (
+        <div
+          className="description"
+          dangerouslySetInnerHTML={{ __html: description }}
+        />
+      )}
+      {((validationMessage && touched) || error) && (
         <span className="error-message" id={`error_${formId}_${id}`}>
-          {validationMessage}
+          {validationMessage || error}
         </span>
       )}
-    </div>
+    </Box>
   );
 };
 
